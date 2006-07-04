e46eec717cafa896005064cd69a8f631ee5039bf
kvprasanth <prasanth.git@gmail.com>
2006-07-04 12:58:17 -0700

    lab - 1

---
+++ BINS.C
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+{
+ int arr[100],i,j,e,m;
+ clrscr();
+ cout<<"Enter array size:";
+ cin>>m;
+ for(i=0;i<m;i++)
+ {
+  cout<<"Element["<<i<<"]:-";
+  cin>>arr[i];
+ }
+
+
+}
