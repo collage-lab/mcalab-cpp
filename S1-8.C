1dc1e93728bcd0fa42a9ee7a2c9a8bcc2d5deb57
kvprasanth <prasanth.git@gmail.com>
2006-08-07 12:55:35 -0700

    lab-7

---
+++ S1-8.C
@@ -0,0 +1,14 @@
+//c=(f-32)5/9;
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+ float c,f;
+ clrscr();
+ cout<<"Enter Temperature:[F]:-";
+ cin>>f;
+ cout<<"\n\nTemp. in Fahrenheit:-"<<((f-32)/(5/9));
+ getch();
+
+}
